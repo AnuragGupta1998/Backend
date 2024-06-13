a94d7072f66b7f6752558d3f4c3cd7370f240594
Anurag Gupta <anurag6967@gmail.com>
2024-06-13 14:11:48 +0530

    some updated into deleteFromCloudinary.js

--- src/utils/deleteFromCloudinary.js
+++ src/utils/deleteFromCloudinary.js
@@ -1,7 +1,13 @@
 import { v2 as cloudinary } from "cloudinary";
 
+// cloudinary.config({
+//     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+//     api_key: process.env.CLOUDINARY_API_KEY,
+//     api_secret: process.env.CLOUDINARY_API_SECRET,
+//   });
+
 // import { json } from "express";
-import { ApiResponse } from "./ApiResponse";
+import { ApiResponse } from "./ApiResponse.js";
 
 const deleteImageCloudinary = async (publicId)=>{
     cloudinary.uploader.destroy(publicId,{resource_type:auto})
