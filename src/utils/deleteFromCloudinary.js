da74b3ba9600a68c57c36a8c07d98573901a6741
Anurag Gupta <anurag6967@gmail.com>
2024-06-13 11:14:59 +0530

    arguments changes in deleteFromCloudinary.js function

--- src/utils/deleteFromCloudinary.js
+++ src/utils/deleteFromCloudinary.js
@@ -3,11 +3,11 @@ import { v2 as cloudinary } from "cloudinary";
 // import { json } from "express";
 import { ApiResponse } from "./ApiResponse";
 
-const deleteImageFromCloudinary = async (oldImagePath)=>{
-    cloudinary.uploader.destroy(oldImagePath,{resource_type:auto})
+const deleteImageCloudinary = async (publicId)=>{
+    cloudinary.uploader.destroy(publicId,{resource_type:auto})
 
     // return json(new ApiResponse)
     return json(new ApiResponse(200,{},"Old image of User has been deleted from cloudinary"))
 
 }
-export {deleteImageFromCloudinary}
+export {deleteImageCloudinary}
